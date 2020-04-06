ff647164e337ce15069ef3686c087040f4672ac3
Amr Elganzory <43642001+AmrGanz@users.noreply.github.com>
2020-04-06 22:59:28 +0400

    Create top_pods_by_cpu_uasge.sh

---
+++ top_pods_by_cpu_uasge.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Get top 10 CPU demanding PODs
+echo "==================================="
+echo "===============CPU================="
+echo "==================================="
+echo "Please enter the total number of top processes between 1 and 100"
+read COUNT
+while :; do
+	if (( $COUNT <= 0 || $COUNT > 100 )); then
+    		echo "in loop"
+    		echo "Please enter the total number of top processes between 1 and 100"
+    		read COUNT
+	else
+		break
+	fi
+done
+
+PID=(`sort -rnk3 sos_commands/process/ps_auxwww | awk '{print $2}'`)
+
+for i in ${PID[*]}
+do
+        CRIO=`grep -s crio- -m1 proc/$i/mountinfo | cut -d "-" -f5 | cut -d"." -f1`
+        if [ -n "${CRIO// }" ] && [ $COUNT -gt 0 ]
+	then
+	  echo "==================================="
+          echo "ProcessID $i"
+          POD=`grep -s "pod.name\"" sos_commands/runc/runc_state_$CRIO | awk '{print $2}' | sed 's/"//g' | sed 's/,//g' `
+          echo "Pod Name: $POD"
+          NAMESPACE=`grep -s "pod.namespace\"" sos_commands/runc/runc_state_$CRIO | awk '{print $2}' | sed 's/"//g' | sed 's/,//g'`
+          echo "namespace: $NAMESPACE"
+	  echo "CPU utilization: `grep $i sos_commands/process/ps_auxwww | awk '{print $3}'`%"
+	  echo "==================================="
+	  COUNT=$((COUNT-1))
+        fi
+done
