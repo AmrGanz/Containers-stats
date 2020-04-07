c09ea8271abd8bf6c0bf5e8b93dc2bcf620d27b4
Amr Elganzory <43642001+AmrGanz@users.noreply.github.com>
2020-04-07 09:45:48 +0400

    total amount of consumed memory
    Added the total amount of consumed memory of those pods at the end

--- top_pods_by_memory_uasge.sh
+++ top_pods_by_memory_uasge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "==================================="
-echo "=============Memory================"
+echo "==============MEMORY==============="
 echo "==================================="
-echo "Please enter the total number of top processes between 1 and 100"
+echo "Please enter the total number of top processes you require: between 1 and 100"
 read COUNT
 while :; do
 	if (( $COUNT <= 0 || $COUNT > 100 )); then
@@ -15,7 +15,7 @@ while :; do
 done
 
 PID=(`sort -rnk6 sos_commands/process/ps_auxwww | awk '{print $2}'`)
-
+TOTALMEM=0
 for i in ${PID[*]}
 do
         CRIO=`grep -s crio- -m1 proc/$i/mountinfo | cut -d "-" -f5 | cut -d"." -f1`
@@ -28,8 +28,11 @@ do
           NAMESPACE=`grep -s "pod.namespace\"" sos_commands/runc/runc_state_$CRIO | awk '{print $2}' | sed 's/"//g' | sed 's/,//g'`
           echo "namespace: $NAMESPACE"
 	  echo "Memory utilization: `grep $i sos_commands/process/ps_auxwww | awk '{print $4}'`%"
-	  echo "RSS: $(echo "scale=4; $(( `grep $i sos_commands/process/ps_auxwww | awk '{print $6}'` ))/1048576 " | bc -l) GB"
+	  RSS=$(echo "scale=4; $(( `grep $i sos_commands/process/ps_auxwww | awk '{print $6}'` ))/1048576 " | bc -l)
+	  echo "RSS: $RSS GB"
+	  TOTALMEM=`echo "$TOTALMEM + $RSS" | bc`
 	  echo "==================================="
 	  COUNT=$((COUNT-1))
         fi
 done
+echo "Total MEM consumption of $PODS is $TOTALMEM GB"
