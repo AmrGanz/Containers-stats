38b5a7e61147a8b733b015d4be15bded0b4d1e13
Amr Elganzory <43642001+AmrGanz@users.noreply.github.com>
2020-07-15 11:57:11 +0400

    fixed missing inspect file error
    pods that have no inspect file will not show errors

--- pods-info.sh
+++ pods-info.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-CONTAINERS=`cat sos_commands/crio/crictl_ps | awk '{print $1}' | grep -v CONTAINER`
+SOSPATH=$1
+CONTAINERS=`cat $SOSPATH/sos_commands/crio/crictl_ps | grep  Running | awk '{print $1}' | grep -v CONTAINER`
 
 for i in $CONTAINERS ; do
-	CONT=`ls sos_commands/crio/ | grep $i | grep inspect`
-	NAMESPACE=$(cat sos_commands/crio/$CONT |  jq -r '.status.labels."io.kubernetes.pod.namespace"')
-	PODNAME=$(cat sos_commands/crio/$CONT |  jq -r '.status.labels."io.kubernetes.pod.name"')
-	CONTNAME=$(cat sos_commands/crio/$CONT |  jq -r '.status.labels."io.kubernetes.container.name"')
-	CONTPID=$(cat sos_commands/crio/$CONT |  jq -r .pid)
-	CPU=$(cat sos_commands/process/ps_auxwww | awk '{print $2, $3}' | grep -w $CONTPID | awk '{print $2}')
-	MEM=$(cat sos_commands/process/ps_auxwww | awk '{print $2, $4}' | grep -w $CONTPID | awk '{print $2}')
-	echo "=============="
-	echo "Project Name: $NAMESPACE"
-	echo "Pod Name: $PODNAME"
-	echo "Container name: $CONTNAME"
-	echo "Container PID: $CONTPID"
-	echo "CPU: $CPU %"
-	echo "MEM: $MEM %"
+        CONT=`ls $SOSPATH/sos_commands/crio/ | grep $i | grep inspect`
+        if [ -z "$CONT" ]
+        then
+                echo "POD: $i has no inspect file under $SOSPATH/sos_commands/crio/"
+        else
+                NAMESPACE=$(cat $SOSPATH/sos_commands/crio/$CONT |  jq -r '.status.labels."io.kubernetes.pod.namespace"')
+                PODNAME=$(cat $SOSPATH/sos_commands/crio/$CONT |  jq -r '.status.labels."io.kubernetes.pod.name"')
+                CONTNAME=$(cat $SOSPATH/sos_commands/crio/$CONT |  jq -r '.status.labels."io.kubernetes.container.name"')
+                CONTPID=$(cat $SOSPATH/sos_commands/crio/$CONT |  jq -r .pid)
+                CPU=$(cat $SOSPATH/sos_commands/process/ps_auxwww | awk '{print $2, $3}' | grep -w $CONTPID | awk '{print $2}')
+                MEM=$(cat $SOSPATH/sos_commands/process/ps_auxwww | awk '{print $2, $4}' | grep -w $CONTPID | awk '{print $2}')
+                echo "=============="
+                echo "Project Name: $NAMESPACE"
+                echo "Pod Name: $PODNAME"
+                echo "Container name: $CONTNAME"
+                echo "Container PID: $CONTPID"
+                echo "CPU: $CPU %"
+                echo "MEM: $MEM %"
+        fi
 done
